aece8f4cf8a17761db0a9d0503ffb8ed87968e64
Damien Tailhades <damien.tailhades@youboox.fr>
2022-01-22 19:13:35 +0100

    🔨  remove download button on audio player

--- components/AudioPlayer.vue
+++ components/AudioPlayer.vue
@@ -1,41 +1,41 @@
 <template>
   <section class="music">
-    <audio class="player" controls>
+    <audio class="player" controls controlsList="nodownload">
       <source :src="require('@/assets/audio/ada-ya.mp3').default" type="audio/mpeg">
       <source :src="require('@/assets/audio/ada-ya.ogg').default" type="audio/ogg">
       <!-- fallback for non supporting browsers goes here -->
       <p>Your browser does not support HTML5 audio, but you can still.</p>
     </audio>
 
-    <audio class="player" controls>
+    <audio class="player" controls controlsList="nodownload">
       <source :src="require('@/assets/audio/loop.mp3').default" type="audio/mpeg">
       <source :src="require('@/assets/audio/loop.ogg').default" type="audio/ogg">
       <!-- fallback for non supporting browsers goes here -->
       <p>Your browser does not support HTML5 audio, but you can still.</p>
     </audio>
 
-    <audio class="player" controls>
+    <audio class="player" controls controlsList="nodownload">
       <source :src="require('@/assets/audio/street-lobsters-ansstaplitouilles.mp3').default" type="audio/mpeg">
       <source :src="require('@/assets/audio/street-lobsters-ansstaplitouilles.ogg').default" type="audio/ogg">
       <!-- fallback for non supporting browsers goes here -->
       <p>Your browser does not support HTML5 audio, but you can still.</p>
     </audio>
 
-    <audio class="player" controls>
+    <audio class="player" controls controlsList="nodownload">
       <source :src="require('@/assets/audio/street-lobsters-rap.mp3').default" type="audio/mpeg">
       <source :src="require('@/assets/audio/street-lobsters-rap.ogg').default" type="audio/ogg">
       <!-- fallback for non supporting browsers goes here -->
       <p>Your browser does not support HTML5 audio, but you can still.</p>
     </audio>
 
-    <audio class="player" controls>
+    <audio class="player" controls controlsList="nodownload">
       <source :src="require('@/assets/audio/street-lobsters.mp3').default" type="audio/mpeg">
       <source :src="require('@/assets/audio/street-lobsters.ogg').default" type="audio/ogg">
       <!-- fallback for non supporting browsers goes here -->
       <p>Your browser does not support HTML5 audio, but you can still.</p>
     </audio>
 
-    <audio class="player" controls>
+    <audio class="player" controls controlsList="nodownload">
       <source :src="require('@/assets/audio/synthesizer.mp3').default" type="audio/mpeg">
       <source :src="require('@/assets/audio/synthesizer.ogg').default" type="audio/ogg">
       <!-- fallback for non supporting browsers goes here -->
