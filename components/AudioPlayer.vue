371a6336063e31715fd5ecfc1595b70448ebabb6
Damien Tailhades <damien.tailhades@youboox.fr>
2022-01-24 09:13:05 +0100

    🏗️ better audio players generation

--- components/AudioPlayer.vue
+++ components/AudioPlayer.vue
@@ -1,43 +1,8 @@
 <template>
   <section class="music">
-    <audio class="player" controls controlsList="nodownload">
-      <source :src="require('@/assets/audio/ada-ya.mp3').default" type="audio/mpeg">
-      <source :src="require('@/assets/audio/ada-ya.ogg').default" type="audio/ogg">
-      <!-- fallback for non supporting browsers goes here -->
-      <p>Your browser does not support HTML5 audio, but you can still.</p>
-    </audio>
-
-    <audio class="player" controls controlsList="nodownload">
-      <source :src="require('@/assets/audio/loop.mp3').default" type="audio/mpeg">
-      <source :src="require('@/assets/audio/loop.ogg').default" type="audio/ogg">
-      <!-- fallback for non supporting browsers goes here -->
-      <p>Your browser does not support HTML5 audio, but you can still.</p>
-    </audio>
-
-    <audio class="player" controls controlsList="nodownload">
-      <source :src="require('@/assets/audio/street-lobsters-ansstaplitouilles.mp3').default" type="audio/mpeg">
-      <source :src="require('@/assets/audio/street-lobsters-ansstaplitouilles.ogg').default" type="audio/ogg">
-      <!-- fallback for non supporting browsers goes here -->
-      <p>Your browser does not support HTML5 audio, but you can still.</p>
-    </audio>
-
-    <audio class="player" controls controlsList="nodownload">
-      <source :src="require('@/assets/audio/street-lobsters-rap.mp3').default" type="audio/mpeg">
-      <source :src="require('@/assets/audio/street-lobsters-rap.ogg').default" type="audio/ogg">
-      <!-- fallback for non supporting browsers goes here -->
-      <p>Your browser does not support HTML5 audio, but you can still.</p>
-    </audio>
-
-    <audio class="player" controls controlsList="nodownload">
-      <source :src="require('@/assets/audio/street-lobsters.mp3').default" type="audio/mpeg">
-      <source :src="require('@/assets/audio/street-lobsters.ogg').default" type="audio/ogg">
-      <!-- fallback for non supporting browsers goes here -->
-      <p>Your browser does not support HTML5 audio, but you can still.</p>
-    </audio>
-
-    <audio class="player" controls controlsList="nodownload">
-      <source :src="require('@/assets/audio/synthesizer.mp3').default" type="audio/mpeg">
-      <source :src="require('@/assets/audio/synthesizer.ogg').default" type="audio/ogg">
+    <audio v-for="audio in audioList" :key="audio.id" class="player" controls controlsList="nodownload">
+      <source :src="require(`@/assets/audio/${audio.title}.mp3`).default" type="audio/mpeg">
+      <source :src="require(`@/assets/audio/${audio.title}.ogg`).default" type="audio/ogg">
       <!-- fallback for non supporting browsers goes here -->
       <p>Your browser does not support HTML5 audio, but you can still.</p>
     </audio>
@@ -46,13 +11,18 @@
 
 <script>
 export default {
-  // data() {
-  //   return {
-  //     audioList: {
-  //       0:
-  //     }
-  //   }
-  // }
+  data() {
+    return {
+      audioList: [
+        { id: 0, title: "ada-ya"},
+        { id: 1, title: "loop"},
+        { id: 2, title: "street-lobsters-ansstaplitouilles"},
+        { id: 3, title: "street-lobsters-rap"},
+        { id: 4, title: "street-lobsters"},
+        { id: 5, title: "synthesizer"},
+      ],
+    }
+  }
 }
 </script>
 
